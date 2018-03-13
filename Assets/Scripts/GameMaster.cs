fbf4c112d926c3893597a46f5f6e192238a11a59
Unknown <edgar.i840@gmail.com>
2018-03-13 18:33:49 +0100

    Door done!

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster GM;
+
+    public float numberKeys = 3;
+    public float keysPicked = 0;
+
+    private void Start()
+    {
+        if (GM != null)
+            GameObject.Destroy(GM);
+        else
+            GM = this;
+    }
+
+    public void AddKey()
+    {
+        keysPicked++;
+
+        if (keysPicked >= numberKeys)
+        {
+            Door.door.doorOpened = true;
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+
+    }
+}
