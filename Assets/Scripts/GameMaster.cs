9d26df8c974cd9fb4bc9e23c7b45df45ec36bb48
Unknown <edgar.i840@gmail.com>
2018-03-13 23:29:50 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
@@ -10,20 +11,33 @@ public class GameMaster : MonoBehaviour
     private float numberKeys = 3;
     private float keysPicked = 0;
 
+    public Text keyText;
+    public GameObject fadeIn;
+    public GameObject fadeOut;
+
     private void Start()
     {
         if (GM != null)
             GameObject.Destroy(GM);
         else
             GM = this;
+
+        fadeIn = GameObject.Find("FadeIn");
+        fadeOut = GameObject.Find("FadeOut");
+        keyText = GameObject.Find("KeyText").GetComponent<Text>();
+
+        keyText.text = "0/" + numberKeys.ToString();
     }
 
     public void AddKey()
     {
         keysPicked++;
 
+        keyText.text = keysPicked.ToString() + "/" + numberKeys.ToString();
+
         if (keysPicked >= numberKeys)
         {
+            keyText.color = Color.yellow;
             Door.door.doorOpened = true;
         }
     }
