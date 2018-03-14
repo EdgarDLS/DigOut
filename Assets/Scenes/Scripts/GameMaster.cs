c9dc26061823eb25fbd63c256000b6a222384eff
Unknown <albert_bpozo@hotmail.es>
2018-03-14 01:44:28 +0100

    Arreglao

--- Assets/Scenes/Scripts/GameMaster.cs
+++ Assets/Scenes/Scripts/GameMaster.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:Assets/Scripts/GameMaster.cs
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -85,54 +84,3 @@ public class GameMaster : MonoBehaviour
         SceneManager.LoadScene(level, LoadSceneMode.Single);
     }
 }
-=======
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-public class GameMaster : MonoBehaviour
-{
-    public static GameMaster GM;
-
-    public int levelToLoad;
-
-    private float numberKeys = 3;
-    private float keysPicked = 0;
-
-    public Text keyText;
-    public GameObject fadeIn;
-    public GameObject fadeOut;
-
-    private void Start()
-    {
-        if (GM != null)
-            GameObject.Destroy(GM);
-        else
-            GM = this;
-
-        fadeIn = GameObject.Find("FadeIn");
-        fadeOut = GameObject.Find("FadeOut");
-        keyText = GameObject.Find("KeyText").GetComponent<Text>();
-
-        keyText.text = "0/" + numberKeys.ToString();
-    }
-
-    public void AddKey()
-    {
-        keysPicked++;
-
-        keyText.text = keysPicked.ToString() + "/" + numberKeys.ToString();
-
-        if (keysPicked >= numberKeys)
-        {
-            keyText.color = Color.yellow;
-            Door.door.doorOpened = true;
-        }
-    }
-
-    public void LoadNextLevel()
-    {
-        SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
-    }
-}
->>>>>>> Stashed changes:Assets/Scenes/Scripts/GameMaster.cs
