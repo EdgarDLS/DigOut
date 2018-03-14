3d4c29c5e560d9ea2ab44647519e6b3df1555907
Unknown <albert_bpozo@hotmail.es>
2018-03-14 03:08:19 +0100

    MorePowerUps

---
+++ Assets/Scenes/Scripts/BallSpeedUpScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeedUpScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag.Equals("Player"))
+        {
+            Drill.drill.makeBig = true;
+            Destroy(this.gameObject);
+        }
+
+    }
+}
